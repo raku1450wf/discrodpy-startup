8d5a0317cd364b603af95e8dc5acb8a83718be16
raku1450wf <77784982+raku1450wf@users.noreply.github.com>
2021-06-06 19:25:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,50 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'ODQzMzQ1Mjk1MzUzMDUzMTg1.YKCggw.8wzkVY9QHIvdZhm8LZOVQf7CMVg'
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    print("Botは正常に起動しました！")
+    print(client.user.name)  # Botの名前
+    print(client.user.id)  # ID
+    print(discord.__version__)  # discord.pyのバージョン
+    print('------')
+#BOTのプレイ表記
+    await client.change_presence(activity=discord.Game(name="開発中(らく)"))
 
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+#コマンド受信時に動作する処理
+    #コマンド「/link m」と入力したらミルダムリンクが返る処理
+    if message.content == '/link m':
+        await message.channel.send('https://www.mildom.com/profile/11053439')
+    #コマンド「/link y」と入力したらYoutubeリンクが返る処理
+    if message.content == '/link y':
+        await message.channel.send('https://www.youtube.com/channel/UCKOtOcXnTyaglPDo1_9OaGg')
+    #コマンド「/link t」と入力したらツイッターリンクが返る処理
+    if message.content == '/link t':
+        await message.channel.send('https://twitter.com/mukimukidamasii')
 
+#管理者専用コマンド受信時に動作する処理
+    #コマンド「/live 9」と入力したら配信予告が返る処理(9時から配信)
+    if message.content == '/live 9':
+        await message.channel.send('@everyone 21時から配信です！ https://www.mildom.com/profile/11053439')
+    #コマンド「/live ?」と入力したら不定期配信予告が返る処理(何かしらアクシデントがあった場合)
+    if message.content == '/live ?':
+        await message.channel.send('@everyone もうすぐで配信です！ https://www.mildom.com/profile/11053439')
 
-bot.run(token)
+client.run(TOKEN)
